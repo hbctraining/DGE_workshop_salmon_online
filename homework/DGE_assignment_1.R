dbdfcf78119c9ebee7d1c00a43c8e8778b17b368
Jihe Liu <31855167+jihe-liu@users.noreply.github.com>
2020-06-15 12:36:22 -0400

    Add files via upload

---
+++ homework/DGE_assignment_1.R
@@ -0,0 +1,25 @@
+#### Assignment 1 ####
+
+#### RNA-seq counts distribution
+# 1. Evaluate the relationship between mean and variance for the control replicates (Irrel_kd samples). Note the differences or similarities in the plot compared to the one using the overexpression replicates.
+
+
+# 2. An RNA-seq experiment was conducted on mice forebrain to evaluate the effect of increasing concentrations of a treatment. For each of the five different concentrations we have n = 5 mice for a total of 25 samples. If we observed little to no variability between replicates, what might this suggest about our samples?
+
+
+# 3. What type of mean-variance relationship would you expect to see for this dataset?
+
+
+#### Count normalization
+# 1. Suppose we have sample names matching in the counts matrix and metadata file, but they are in different order. Write the line(s) of code to create a new matrix with columns re-ordered such that they are identical to the row names of the metadata.
+
+
+#### Sample-level QC
+# 1. What does the above plot tell you about the similarity of samples?
+
+
+# 2. Does it fit the expectation from the experimental design?
+
+
+# 3. What do you think the %variance information (in the axes titles) tell you about the data in the context of the PCA?
+
